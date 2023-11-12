f5b1d5b321a99fc2bea1ca9b30dd52785e59dfba
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-11-12 13:13:28 +0530

    Change in script.js
    - Add feature to change style input element of id password when match with input element of id conform password
    - Create getValidPassword ,
    getInvalidPassword and checkValidPassword functions to change style of input element of type password

--- script.js
+++ script.js
@@ -4,3 +4,39 @@ let passwordConformation = document.querySelector(
   ".form-one div:nth-child(3) > p "
 );
 let temp;
+
+let getValidPassword = () => {
+  passwordInputElement.classList.remove("error");
+  conformPasswordInputElement.classList.remove("error");
+
+  passwordInputElement.classList.add("valid");
+  conformPasswordInputElement.classList.add("valid");
+
+  passwordConformation.innerText = "*Password do match";
+
+  passwordConformation.style.color = "green";
+};
+
+let getInvalidPassword = () => {
+  passwordInputElement.classList.remove("valid");
+  conformPasswordInputElement.classList.remove("valid");
+
+  passwordInputElement.classList.add("error");
+  conformPasswordInputElement.classList.add("error");
+
+  passwordConformation.innerText = "*Password do not match";
+
+  passwordConformation.style.color = "red";
+};
+
+let checkValidPassword = () => {
+  if (temp === conformPasswordInputElement.value) {
+    getValidPassword();
+  } else if (temp !== conformPasswordInputElement.value) {
+    getInvalidPassword();
+  }
+};
+
+conformPasswordInputElement.onkeyup = () => {
+  return checkValidPassword();
+};
