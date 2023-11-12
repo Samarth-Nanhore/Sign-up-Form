cf4d15ee12e07919643bd1374270567765113b84
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-11-12 13:04:49 +0530

    Change in index.html and style.css and script.js
    - Add new class named error to input element type password
    - Create DOM variable in script.js for accessing html element

---
+++ script.js
@@ -0,0 +1,6 @@
+let passwordInputElement = document.getElementById("password");
+let conformPasswordInputElement = document.getElementById("conform-password");
+let passwordConformation = document.querySelector(
+  ".form-one div:nth-child(3) > p "
+);
+let temp;
